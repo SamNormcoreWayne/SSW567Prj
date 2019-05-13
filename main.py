696d619d41c6e30a7371bbc07a6f175cb4ecc337
Ziyu Zhang <zcvszy@gmail.com>
2019-05-13 13:23:09 -0400

    commit before leave;

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ from foo.repository import Repository
     Attribute: (dict(Family), working_path, filename)
     Methods: (input_family(), output_family())
 '''
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
