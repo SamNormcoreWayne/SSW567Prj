aa22b0b15cec3a400bd08d8ab28984c311d31d2f
Sherman77 <44691915+Sherman77@users.noreply.github.com>
2019-05-14 04:13:14 -0400

    Implemented all 6 user stories
    Implemented all 6 user stories

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ from foo.repository import Repository
     Methods: (input_family(), output_family())
 '''
 
+
 def main():
     dir = os.path.join(os.getcwd(), "docs")
     filename = "sample"
     repo = Repository(filename, dir)
     repo.output_family()
+    repo.income_more_than_30k()
+    repo.fewer_than_10_kids()
+    repo.younger_than_5()
+    repo.check_benefit_eligibility()
 
 if __name__ == "__main__":
     main()
